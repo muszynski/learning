838b00c292e7b29c6d07b569e90b656ebaca64a4
muszynski <muszynski@gmail.com>
2023-02-17 19:19:06 +0100

    init commit
    init

---
+++ learning/learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Łukasz Muszyński on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
